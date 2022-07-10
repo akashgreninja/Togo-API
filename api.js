2132fe25b257f80af34e1cd9eac6a35e9f6e597d
Akash <akashuhulekal@gmail.com>
2022-07-10 19:49:16 +0530

    hola

--- api.js
+++ api.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 
 const router = express.Router();
@@ -6,7 +7,9 @@ router.get("/get-photos",async(req,res)=>{
     try{
 
         const photos ={
-            photos:[{
+            photos:[
+                //from here
+                {
                 id: 12441544,
 
             src:{
@@ -14,7 +17,28 @@ router.get("/get-photos",async(req,res)=>{
                 medium:"https://images.pexels.com/photos/12772076/pexels-photo-12772076.jpeg?auto=compress&cs=tinysrgb&h=350"
 
             }
-        }]
+        },
+        {
+            id: 12441544,
+
+        src:{
+            original:"https://images.pexels.com/photos/12772651/pexels-photo-12772651.png",
+            medium:"https://images.pexels.com/photos/12772651/pexels-photo-12772651.png?auto=compress&cs=tinysrgb&h=350"
+
+        }
+    },
+    {
+        id: 12441544,
+
+    src:{
+        original:"https://images.pexels.com/photos/12772650/pexels-photo-12772650.png",
+        medium:"https://images.pexels.com/photos/12772650/pexels-photo-12772650.png?auto=compress&cs=tinysrgb&h=350"
+
+    }
+},
+        
+    
+    ]
 
         }
         res.json(photos);
