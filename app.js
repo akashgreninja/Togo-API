b0e27a5b2dd6c2fb4df7b609520ce10a29944c85
akashgreninja <akashuhulekal@gmail.com>
2022-09-13 23:08:30 +0530

    changes1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+require("dotenv").config();
+const port = process.env.THE_PORT
 // const port=5000
 var cors = require('cors')
+
 
 app.use(cors())
 app.use(express.json())
